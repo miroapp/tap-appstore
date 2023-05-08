6918c91fa5a69e5658ce577a7aab1770207988da
Joao Amaral <7281460+joaopamaral@users.noreply.github.com>
2023-05-08 16:54:40 -0300

    fixes

--- tap_appstore/sync.py
+++ tap_appstore/sync.py
@@ -7,18 +7,16 @@ from tap_appstore.streams import STREAMS
 def get_selected_streams(catalog):
     selected_streams = []
     for stream in catalog['streams']:
-        stream_metadata = stream.metadata
-        for entry in stream_metadata:
+        for entry in stream['metadata']:
             # Stream metadata will have an empty breadcrumb
             if not entry['breadcrumb'] and entry['metadata'].get('selected', None):
-                selected_streams.append((stream.tap_stream_id, stream.to_dict()))
-
+                selected_streams.append(stream)
     return selected_streams
 
 
 def sync(client: Api, config, state, catalog):
-    # Write all schemas and init count to 0
-    for stream_name, catalog_entry in get_selected_streams(catalog):
+    for catalog_entry in get_selected_streams(catalog):
+        stream_name = catalog_entry['tap_stream_id']
         stream_obj = STREAMS[stream_name](client, config, state)
         singer.write_schema(stream_name, catalog_entry['schema'], catalog_entry['key_properties'])
         stream_obj.query_report(catalog_entry)
